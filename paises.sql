13bde58deaadf69287718f27726f00afe2281629
Hernán Alperin <hernan.alperin@gmail.com>
2017-11-03 10:56:25 -0300

    Update paises.sql

--- paises.sql
+++ paises.sql
@@ -1,9 +1,18 @@
+-----------------------------------------------------------------------
+-- OJO ESTO NO VALE, HAY QUE DISTINGUIR POR NOMBRES LOS REPETIDOS    --
+-- Alvarez, Álvarez, ALVAREZ, etc
+-----------------------------------------------------------------------
+
+(!) No. Hay que selecionar asistentes distintos
+
 select "Ciudad y País de Residencia", count(*)
 from asistentes
 where "Estado del participante" in ('Acreditado', 'Participa', 'Cambiado')
 group by "Ciudad y País de Residencia"
 order by count desc
 ;
+
+
 
         Ciudad y País de Residencia         | count
 --------------------------------------------+-------
@@ -34,3 +43,43 @@ select count(*) from asistentes;
   1723
 
 -- 1047 de 1723 no sabemos de donde vinieron ... más del 60%
+
+
+:-P
+
+copiando y pegando, cherry picking...
+ United States                              |     8
+ Santiago                                   |     7     (asumo Chile)
+ Santiago, Chile                            |     5
+ Ellensburg, estado de Washington, EEUU     |     4
+ Brasil                                     |     3
+ Pato Branco - BRASIL                       |     3
+ Santiago de Chile                          |     2
+ USA                                        |     1
+ Ellensburg, Washington, Estados Unidos     |     1
+ Melipilla - Chile                          |     1
+ Asunción de Paraguay                       |     1
+ Sevilla                                    |     1
+ Guatemala                                  |     1
+ Managua                                    |     1
+
+
+
+--Hay que selecionar asistentes distintos
+
+with distintos as (
+  select lower("Apellido"), lower("Nombre"), count(*)
+  from asistentes
+  where "Estado del participante" in ('Acreditado', 'Participa', 'Cambiado')
+  and substr("Fecha de pedido",1,4) = '2017'
+  group by lower("Apellido"), lower("Nombre")
+  order by lower("Apellido"), lower("Nombre")
+  )
+        
+select "Ciudad y País de Residencia", count(*)
+from asistentes
+where "Estado del participante" in ('Acreditado', 'Participa', 'Cambiado')
+group by "Ciudad y País de Residencia"
+order by count desc
+;
+
