f45f540d76207be8ae06cf3c64bc9e3db0aad73a
Hernán Alperin <hernan.alperin@gmail.com>
2017-12-19 18:03:31 -0300

    Update paises.sql

--- paises.sql
+++ paises.sql
@@ -6,87 +6,96 @@ group by "Ciudad y País de Residencia"
 order by count desc
 ;
 
-
-
 -----------------------------------------------------------------------
 -- OJO ESTO NO VALE, HAY QUE DISTINGUIR POR NOMBRES LOS REPETIDOS    --
 -- Alvarez, Álvarez, ALVAREZ, etc
 -----------------------------------------------------------------------
 
-(!) No. Hay que selecionar asistentes distintos
-
-
-        Ciudad y País de Residencia         | count
---------------------------------------------+-------
-                                            |  1046
- Argentina                                  |    64
- Buenos Aires, Argentina                    |    33
- CABA, Argentina                            |    25
- CABA                                       |    17
- Ciudad de Buenos Aires                     |    16
- CABA - Argentina                           |    13
- Buenos Aires                               |    13
- Rosario, Argentina                         |    12
- Rosario, Santa Fe, Argentina               |    10
- Buenos Aires Argentina                     |     9
- Corrientes Argentina                       |     8
- Mendoza, Argentina                         |     8
- United States                              |     8
- cordoba                                    |     8
- Santiago                                   |     7
- Comodoro Rivadavia                         |     7
- Neuquen, Argentina                         |     7
- CORDOBA                                    |     7
- Córdoba Argentina                          |     5
-
-select count(*) from asistentes;
- count
--------
-  1723
-
--- 1047 de 1723 no sabemos de donde vinieron ... más del 60%
-
-
-:-P
-
-copiando y pegando, cherry picking...
- United States                              |     8
- Santiago                                   |     7     (asumo Chile)
- Santiago, Chile                            |     5
- Ellensburg, estado de Washington, EEUU     |     4
- Brasil                                     |     3
- Pato Branco - BRASIL                       |     3
- Santiago de Chile                          |     2
- USA                                        |     1
- Ellensburg, Washington, Estados Unidos     |     1
- Melipilla - Chile                          |     1
- Asunción de Paraguay                       |     1
- Sevilla                                    |     1
- Guatemala                                  |     1
- Managua                                    |     1
-
-
-
---Hay que selecionar asistentes distintos
-
-with distintos as (
-  select translate(lower("Apellido"),'áéíóúü-, ','aeiouu'), translate(lower("Nombre"),'áéíóúü-, ','aeiouu'), count(*)
-  from asistentes
-  where "Estado del participante" in ('Acreditado', 'Participa', 'Cambiado')
-  and substr("Fecha de pedido",1,4) = '2017'
-  group by lower("Apellido"), lower("Nombre")
-  order by lower("Apellido"), lower("Nombre")
-  )
-select * from distintos
-;
-
-        
-        
-        
-select "Ciudad y País de Residencia", count(*)
+drop table email_asistentes_2017;
+create table email_asistentes_2017 as  -- para determinar únicos
+select distinct "Correo electrónico" as email, "Nombre" as nombre,
+        "Ciudad y País de Residencia" as ciudad_pais
 from asistentes
 where "Estado del participante" in ('Acreditado', 'Participa', 'Cambiado')
-group by "Ciudad y País de Residencia"
-order by count desc
+and substr("Fecha de pedido", 1, 4) = '2017'
 ;
+
+alter table email_asistentes_2017 add column pais text;
+alter table email_asistentes_2017 add column sexo text;
+alter table email_asistentes_2017 add column id serial primary key;
+-- y a editar con pgadmin...
+
+with distintos as (select distinct email, sexo
+        from email_asistentes_2017)
+        select sexo, count(*)
+        from distintos
+        group by sexo
+;
+
+
+ sexo | count
+------+-------
+      |     2
+ F    |   198
+ M    |   347
+(3 filas)
+
+select 100.0*198/(198 + 347);
+select 100.0*347/(198 + 347);
+
+F 36%
+M 64%
+
+with distintos as (select distinct email, pais
+        from email_asistentes_2017)
+        select pais, count(*)
+        from distintos
+        group by pais
+        order by count
+;
+
+ pais | count
+------+-------
+ ni   |     1
+ gt   |     1
+ py   |     1
+ pe   |     2
+ uy   |     2
+ co   |     2
+ mx   |     3
+ es   |     4
+ us   |     5
+ br   |     5
+ cl   |     8
+      |   269
+ ar   |   271
+(13 filas)
+
+with distintos as (select distinct email, pais
+        from email_asistentes_2017)
+        select case 
+                when pais = 'ar' then 'Argentina' 
+                else 'Exterior'        
+                end as donde, 
+                count(*)
+        from distintos
+        where pais is not Null and pais != ''
+        group by case 
+                when pais = 'ar' then 'Argentina' 
+                else 'Exterior'        
+                end
+        order by count
+;
+
+   donde   | count
+-----------+-------
+ Exterior  |    34
+ Argentina |   271
+(2 filas)
+
+select 100.0*34/(34 + 271);
+select 100.0*271/(34 + 271);
+
+Exterior 11%
+Argentina 89%
 
